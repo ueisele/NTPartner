9b57b4f7c3981e8ead0dd4fcd9af02b8f840ed11
pkutschera <peter.kutschera@novatec-gmbh.de>
2017-05-30 21:21:23 +0200

    Changed pom packaging to war

--- deployOpenshift.sh
+++ deployOpenshift.sh
@@ -6,5 +6,5 @@ oc new-project ntpartner
 oc get templates -n openshift
 oc new-app -e MYSQL_USER=dbuser -e MYSQL_PASSWORD=dbuser -e MYSQL_DATABASE=ntpartner openshift/mysql-55-centos7
 oc new-app rabbitmq
-oc new-app wildfly:10.1~/home/user/git/NTPartner-master/ -e SPRING_PROFILES_ACTIVE=openshift --name ntpartner
+oc new-app wildfly:10.1~https://github.com/pkutschera/NTPartner#openshift -e SPRING_PROFILES_ACTIVE=openshift --name ntpartner
 oc expose svc/ntpartner
